c82445188f09ee423b386ab85ccacb9934d2bebc
Jayson Reis <santosdosreis@gmail.com>
2017-10-10 16:38:19 +0200

    zsh fixes

--- roles/zsh/config.zsh
+++ roles/zsh/config.zsh
@@ -2,11 +2,12 @@
 DISABLE_CORRECTION="true"
 unsetopt correct
 unsetopt correct_all
+if test "$(uname -a | grep -i darwin)"; then
+    # ==> Caveats
+    # Add the following to your zshrc to access the online help:
+    unalias run-help
+    autoload run-help
+    HELPDIR=/usr/local/share/zsh/help
 
-# ==> Caveats
-# Add the following to your zshrc to access the online help:
-unalias run-help
-autoload run-help
-HELPDIR=/usr/local/share/zsh/help
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+    test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+fi
