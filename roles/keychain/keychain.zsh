f9f8bdf7b97ce5e59cfbf760e7d91072fb3c85e9
Jayson Reis <santosdosreis@gmail.com>
2018-11-26 10:02:57 +0100

    Change how keychain load custom keys

--- roles/keychain/keychain.zsh
+++ roles/keychain/keychain.zsh
@@ -1,1 +1,8 @@
-eval $(keychain --agents ssh,gpg)
+extra_keys_file=~/.keychain_keys
+if [ -f "${extra_keys_file}" ]; then
+    _EXTRA_KEYS=$(cat $extra_keys_file)
+else
+    _EXTRA_KEYS=""
+    echo "Create the file $extra_keys_file to have the right keys loads and/or to dismiss this message."
+fi
+eval $(keychain --eval --agents ssh,gpg --inherit any id_rsa "$_EXTRA_KEYS")
