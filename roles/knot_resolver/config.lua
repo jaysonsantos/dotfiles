10eff24623752bf80f6faad12e5114c67c1c1c41
Jayson Reis <santosdosreis@gmail.com>
2019-02-27 15:39:52 +0100

    Add a role for knot-resolver

---
+++ roles/knot_resolver/config.lua
@@ -0,0 +1,43 @@
+modules = {
+    'policy',
+    hints = '/etc/hosts',
+    predict = {
+        window = 15, -- 15 minutes sampling window
+        period = 6*(60/15) -- track last 6 hours
+    },
+    'stats',
+    -- http = {
+    --     host = 'localhost',
+    --     port = 8053,
+    -- }
+}
+
+net = { '127.0.0.1' }
+
+-- VPN specific DNS
+{% if knot_resolver_custom_servers %}
+local custom_servers = {
+    {% for ip, servers in knot_resolver_custom_servers.items() %}
+    ['{{ ip }}'] = {
+        {% for server in servers %}
+        '{{ server }}',
+        {% endfor %}
+    },
+    {% endfor %}
+}
+for server, domains in pairs(custom_servers) do
+    for _, domain in pairs(domains) do
+        log('Resolving %s with %s', domain, server)
+        policy.add(policy.suffix(policy.STUB(server), {todname(domain)}))
+    end
+end
+{% endif %}
+
+-- Fallback to Cloudflare TLS
+policy.add(
+    policy.all(
+        policy.TLS_FORWARD({
+            {'1.1.1.1', hostname='1.1.1.1'}
+        })
+    )
+)
