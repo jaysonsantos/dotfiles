44f7a3b2c09f03523907fa61edbe7eee864b74c2
Jayson Reis <santosdosreis@gmail.com>
2019-03-25 15:51:10 +0100

    Configure fzf

--- roles/fzf/_env.zsh
+++ roles/fzf/_env.zsh
@@ -1,2 +1,3 @@
-# Use ag to respect .gitignore
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --color=always'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS="--ansi"
